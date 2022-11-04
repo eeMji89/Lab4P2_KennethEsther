a7834751dc430b7034adbc54d3350eeb0c190b81
eeMji89 <110209389+eeMji89@users.noreply.github.com>
2022-11-04 17:40:46 -0600

    Avance

--- src/lab4p2_kennethesther/Lab4P2_KennethEsther.java
+++ src/lab4p2_kennethesther/Lab4P2_KennethEsther.java
@@ -173,5 +173,53 @@ static ArrayList <Jugadores> jugadores = new ArrayList();
         return ve;
     }
     */
+    public static char [][] jugar(char [][]matriz,ArrayList<Vehiculos>v, ArrayList<Vehiculos>ve){
+        int vn = 0+r.nextInt(v.size()-1);
+       int x=0, y=0;
+        for (int i = 0; i < v.size()-1; i++) {
+            if (v.get(i) instanceof Barcos) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 1;
+                matriz[x][y]='B';
+            }
+            else if (v.get(i) instanceof Aviones) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 0+r.nextInt(v.size()-1);
+                matriz[x][y]='A';
+            }
+            else if (v.get(i) instanceof Submarinos) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 0;
+                matriz[x][y]='S';
+            }  
+        }
+        for (int i = 0; i < ve.size()-1; i++) {
+            if (ve.get(i) instanceof Barcos) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 8;
+                matriz[x][y]='b';
+            }
+            else if (ve.get(i) instanceof Aviones) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 0+r.nextInt(v.size()-1);
+                matriz[x][y]='a';
+            }
+            else if (ve.get(i) instanceof Submarinos) {
+                x= 0+r.nextInt(v.size()-1);
+                y = 9;
+                matriz[x][y]='s';
+            }  
+        }
+       return matriz; 
+    }
+    
+    public char [][]generarM(char [][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j]=' ';
+            }
+        }
+        return matriz;
+    }
     
 }
