d71a2ff399e3d4c036b21e2caa47b96a51bc8dd7
eeMji89 <110209389+eeMji89@users.noreply.github.com>
2022-11-04 13:49:32 -0600

    Avance

--- src/lab4p2_kennethesther/Lab4P2_KennethEsther.java
+++ src/lab4p2_kennethesther/Lab4P2_KennethEsther.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package lab4p2_kennethesther;
+
+import java.util.Scanner;
 
 /**
  *
  * @author ELIZABETH HERNANDEZ
  */
 public class Lab4P2_KennethEsther {
-
+Scanner lea = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       boolean flag = false;
+        while (flag== false) {            
+            System.out.println("MENU");
+            System.out.println("0. Salir");
+            System.out.println("1. Crear jugadores y equipos");
+            System.out.println("2. Jugar");
+            
+            
+        }
+        
+        
+        
     }
     
 }
