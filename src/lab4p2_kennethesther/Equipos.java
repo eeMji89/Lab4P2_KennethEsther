bdd2face37a54a7283a688f5974c58b06aaaf853
eeMji89 <110209389+eeMji89@users.noreply.github.com>
2022-11-04 14:07:32 -0600

    B

--- src/lab4p2_kennethesther/Equipos.java
+++ src/lab4p2_kennethesther/Equipos.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab4p2_kennethesther;
+
+import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  *
  * @author ELIZABETH HERNANDEZ
  */
 public class Equipos {
+    protected Color color;
+    protected int nvictoria;
+    protected ArrayList <Vehiculos> vehiculos = new ArrayList();
+    protected ArrayList<Jugadores> jugadores = new ArrayList();
+
+    public Equipos() {
+    }
+
+    public Equipos(Color color, int nvictoria, ArrayList<Jugadores> jugadores) {
+        this.color = color;
+        this.nvictoria = nvictoria;
+        this.jugadores = jugadores;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getNvictoria() {
+        return nvictoria;
+    }
+
+    public void setNvictoria(int nvictoria) {
+        this.nvictoria = nvictoria;
+    }
+
+    public ArrayList<Vehiculos> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculos> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public ArrayList<Jugadores> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugadores> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipos{" + "color=" + color + ", nvictoria=" + nvictoria + ", vehiculos=" + vehiculos + ", jugadores=" + jugadores + '}';
+    }
+    
     
 }
--- src/lab4p2_kennethesther/Vehiculos.java
+++ src/lab4p2_kennethesther/Vehiculos.java
@@ -1,13 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab4p2_kennethesther;
+
+import java.awt.Color;
 
 /**
  *
  * @author ELIZABETH HERNANDEZ
  */
 public class Vehiculos {
+    protected String nombre;
+    protected int dano;
+    protected int x;
+    protected int y;
+    protected int vida;
+    protected Jugadores conductor;
+    protected Color color;
+
+    public Vehiculos() {
+    }
+
+    public Vehiculos(String nombre, int dano, int x, int y, int vida, Jugadores conductor, Color color) {
+        this.nombre = nombre;
+        this.dano = dano;
+        this.x = x;
+        this.y = y;
+        this.vida = vida;
+        this.conductor = conductor;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDano() {
+        return dano;
+    }
+
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public Jugadores getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Jugadores conductor) {
+        this.conductor = conductor;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculos{" + "nombre=" + nombre + ", dano=" + dano + ", x=" + x + ", y=" + y + ", vida=" + vida + ", conductor=" + conductor + ", color=" + color + '}';
+    }
     
 }
